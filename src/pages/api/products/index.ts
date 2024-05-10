945a54709f7eea773c96ec013f675dfd667c5bfa
biyamn <b1y9mn@gmail.com>
2024-05-10 15:30:54 +0900

    feat: 상품 목록 가져오는 API 추가

---
+++ src/pages/api/products/index.ts
@@ -0,0 +1,50 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+
+type Product = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+};
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    try {
+      const response = await axios.get('https://dummyjson.com/products');
+
+      const filteredProduct = response.data.products.map((product: Product) => {
+        const { id, title, description, price, discountPercentage } = product;
+        const afterDiscountPrice = price - price * (discountPercentage / 100);
+        return {
+          id,
+          title,
+          description,
+          price,
+          discountPercentage,
+          afterDiscountPrice,
+        };
+      });
+
+      const { total, skip, limit } = response.data;
+      const result = {
+        products: filteredProduct,
+        total: total,
+        skip: skip,
+        limit: limit,
+      };
+      return res.status(200).json(result);
+    } catch (error) {
+      console.log('데이터 패칭 오류 발생', error);
+    }
+  }
+}
+
+export default handler;
